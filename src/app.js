bf7c0c591a200055da8028a05f6e5f4f98cc9c56
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-07-06 12:14:37 -0300

    commit 4

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const livros = [
   {id: 1, "titulo": "Senhor dos Aneis"},
   {id: 2, "titulo": "O Hobbit"}
@@ -17,7 +19,17 @@ app.get('/livros', (req, res) => {
 
 app.post('/livros', (req, res) => {
   livros.push(req.body);
-  res.status(201);
+  res.status(201).send("Livro cadastrado com sucesso");
 });
+
+app.put('/livros/:id', (req, res) => {
+  let index = buscaLivro(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+});
+
+function buscaLivro(id) {
+  return livros.findIndex(livro => livro.id == id);
+}
 
 export default app;
