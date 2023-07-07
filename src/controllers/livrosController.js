5454a8e686f8309ca607e332e7bacde130ade7a1
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-07-07 10:01:33 -0300

    commit 9

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,19 +1,44 @@
-import livros from "./models/Livro.js";
+import livros from "../models/Livro.js";
 
 class LivroController {
-  static listarLivros = (req, res) => {
+  static listarLivros = async (req, res) => {
     /* Antigo */
     // livros.find((err, livros) => {
     //   res.status(200).json(livros);
     // });
-    livros.find()
-    .then((livros) => {
-      res.status(200).json(livros);
-    })
-    .catch((erro) => {
-      console.error(erro);
-      res.status(500).json({error: "Ocorreu um erro ao buscar os livros."});
-    });
+
+    /* Novo 1 */
+    // livros.find()
+    // .then((livros) => {
+    //   res.status(200).json(livros);
+    // })
+    // .catch((erro) => {
+    //   console.error(erro);
+    //   res.status(500).json({error: "Ocorreu um erro ao buscar os livros."});
+    // });
+
+    /* Novo 2 */
+    try {
+      const listaLivros = await livros.find();
+      res.status(200).json(listaLivros);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({erro: "Ocorreu um erro ao buscar os livros."});
+    }
+  }
+
+  static cadastrarLivro = async (req, res) => {
+    try {
+      let livro = new livros(req.body);
+      await livro.save();
+      res.status(201).send(livro.toJSON());
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        erro: "Falha ao cadastrar livro.",
+        message: `${error.message}`,
+      });
+    }
   }
 }
 
