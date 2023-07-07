3966dd3bb41ad221c53780ecb37671a394a0bc66
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-07-07 10:25:06 -0300

    commit 10

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -19,11 +19,25 @@ class LivroController {
 
     /* Novo 2 */
     try {
-      const listaLivros = await livros.find();
-      res.status(200).json(listaLivros);
+      const listaDeLivros = await livros.find();
+      res.status(200).json(listaDeLivros);
     } catch (error) {
       console.error(error);
       res.status(500).json({erro: "Ocorreu um erro ao buscar os livros."});
+    }
+  }
+
+  static listarLivroPorId = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const dadosLivro = await livros.findById(id);
+      res.status(200).json(dadosLivro);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        erro: "Id do livro não localizado.",
+        message: `${error.message}`,
+      });
     }
   }
 
@@ -36,6 +50,20 @@ class LivroController {
       console.error(error);
       res.status(500).json({
         erro: "Falha ao cadastrar livro.",
+        message: `${error.message}`,
+      });
+    }
+  }
+
+  static atualizarLivro = async (req, res) => {
+    try {
+      const id = req.params.id;
+      await livros.findByIdAndUpdate(id, { $set: req.body })
+      res.status(200).send({ message: "Livro atualizado com sucesso" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        erro: "Falha ao atualizar livro.",
         message: `${error.message}`,
       });
     }
